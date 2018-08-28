569a3fd4642a616084be9618c71e5ee69df69927
Cornelia Natasha <cornelianatasha528@gmail.com>
2018-08-28 20:04:47 +0700

    hahahahha

--- resources/views/user/welcome.blade.php
+++ resources/views/user/welcome.blade.php
@@ -1,95 +1,272 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html lang="en">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- Title -->
+    <title>KMK ITS</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Favicon -->
+    <link rel="icon" href="img/logo.png">
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Core Stylesheet -->
+    <link href="style.css" rel="stylesheet">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <!-- Responsive CSS -->
+    <link href="css/responsive.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
+</head>
 
-            .position-ref {
-                position: relative;
-            }
+<body>
+    <!-- Preloader Start -->
+    <div id="preloader">
+        <div class="colorlib-load"></div>
+    </div>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+    <!-- ***** Header Area Start ***** -->
+    <header class="header_area animated">
+        <div class="container-fluid">
+       
+                <!-- Menu Area Start -->
+           
+                    <div class="menu_area">
+                        <nav class="navbar navbar-expand-lg navbar-light">
+                            <!-- Logo -->
+                            <a class="navbar-brand" href="#"><img src="img/logo.png" style="height: 100px" id="logonav"></a>
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ca-navbar" aria-controls="ca-navbar" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                            <!-- Menu Area -->
+                            <div class="collapse navbar-collapse" id="ca-navbar">
+                                <ul class="navbar-nav ml-auto" id="nav">
+                                    <li class="nav-item active"><a class="nav-link" href="#home">Home</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#features">Events</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#screenshot">News</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#pricing">Contact Us</a></li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
 
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+    <!-- ***** Wellcome Area Start ***** -->
+     <section class="wellcome_area clearfix" id="home">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-12 col-md">
+                    <div class="wellcome-heading">
+                        <h2>Keluarga Mahasiswa Katholik</h2>
+                        <p>Institut Teknologi Sepuluh Nopember</p>
+                    </div>
+                    <div class="get-start-area">
+                        <!-- Form Start -->
+                        <form action="#" method="post" class="form-inline">
+                            <input type="submit" class="submit" value="Get Started">
+                        </form>
+                        <!-- Form End -->
+                    </div>
                 </div>
             </div>
         </div>
-    </body>
+        <!-- Welcome thumb -->
+        <div class="welcome-thumb wow fadeInDown" data-wow-delay="0.5s">
+            <img src="img/logo.png" alt="">
+        </div>
+    </section>
+    <!-- ***** Wellcome Area End ***** -->
+
+    <!-- ***** Special Area Start ***** -->
+    <section class="special-area bg-white section_padding_100" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <!-- Section Heading Area -->
+                    <div class="section-heading text-center">
+                        <h2>About KMK ITS</h2>
+                        <div class="line-shape"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <!-- Single Special Area -->
+                <div class="col-12 col-md-3">
+                    <div class="single-special text-center wow fadeInUp" data-wow-delay="0.2s">
+                        <div class="single-icon">
+                            <img src="img/history.png">
+                        </div>
+                        <h4>Sejarah KMK</h4>
+                        <p>We build pretty complex tools and this allows us to take designs and turn them into functional quickly and easily</p>
+                    </div>
+                </div>
+                <!-- Single Special Area -->
+                <div class="col-12 col-md-3">
+                    <div class="single-special text-center wow fadeInUp" data-wow-delay="0.4s">
+                        <div class="single-icon">
+                            <img src="img/filosofi-logo.png">
+                        </div>
+                        <h4>Filosofi Logo</h4>
+                        <p>We build pretty complex tools and this allows us to take designs and turn them into functional quickly and easily</p>
+                    </div>
+                </div>
+                <!-- Single Special Area -->
+                <div class="col-12 col-md-3">
+                    <div class="single-special text-center wow fadeInUp" data-wow-delay="0.6s">
+                        <div class="single-icon">
+                            <img src="img/organigram.png">
+                        </div>
+                        <h4>Organigram</h4>
+                        <p>We build pretty complex tools and this allows us to take designs and turn them into functional quickly and easily</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-3">
+                    <div class="single-special text-center wow fadeInUp" data-wow-delay="0.6s">
+                        <div class="single-icon">
+                            <img src="img/gallery.png">
+                        </div>
+                        <h4>Gallry</h4>
+                        <p>We build pretty complex tools and this allows us to take designs and turn them into functional quickly and easily</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+        <section class="special-area bg-white section_padding_100" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <!-- Section Heading Area -->
+                    <div class="section-heading text-center">
+                        <h2>Events on KMK ITS</h2>
+                        <div class="line-shape"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <!-- Single Special Area -->
+                <div class="col-12 col-md-3">
+                    <div class="single-special text-center wow fadeInUp" data-wow-delay="0.2s">
+                        <div class="single-icon">
+                            <img src="img/coming.png">
+                        </div>
+                        <h4>KR</h4>
+                        <p>Kemah Rohani</p>
+                    </div>
+                </div>
+                <!-- Single Special Area -->
+                <div class="col-12 col-md-3">
+                    <div class="single-special text-center wow fadeInUp" data-wow-delay="0.4s">
+                        <div class="single-icon">
+                            <img src="img/coming.png">
+                        </div>
+                        <h4>Natal</h4>
+                        <p>Natal KMK ITS 2018</p>
+                    </div>
+                </div>
+                <!-- Single Special Area -->
+                <div class="col-12 col-md-3">
+                    <div class="single-special text-center wow fadeInUp" data-wow-delay="0.6s">
+                        <div class="single-icon">
+                            <img src="img/coming.png">
+                        </div>
+                        <h4>Paskah</h4>
+                        <p>Paskah KMK ITS 2019</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-3">
+                    <div class="single-special text-center wow fadeInUp" data-wow-delay="0.6s">
+                        <div class="single-icon">
+                            <img src="img/coming.png">
+                        </div>
+                        <h4>3C</h4>
+                        <p>Catholic Cimmunity Cup</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>    
+    <!-- ***** App Screenshots Area Start ***** -->
+    <div class="app-screenshots-area bg-white section_padding_0_100 clearfix" id="screenshot">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <!-- Heading Text  -->
+                    <div class="section-heading">
+                        <h2>News</h2>
+                        <div class="line-shape"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <!-- App Screenshots Slides  -->
+                    <div class="app_screenshots_slides owl-carousel">
+                        <div class="single-shot">
+                            <img src="img/post_1.png" alt="">
+                            <p>selamat hari raya</p>
+                        </div>
+                        <div class="single-shot">
+                            <img src="img/post_2.png" alt="">
+                        </div>
+                        <div class="single-shot">
+                            <img src="img/post_3.png" alt="">
+                        </div>
+                        <div class="single-shot">
+                            <img src="img/post_4.jpg" alt="">
+                        </div>
+                        <div class="single-shot">
+                            <img src="img/post_5.jpg" alt="">
+                        </div>
+                        <div class="single-shot">
+                            <img src="img/post_6.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="footer-social-icon text-center section_padding_70 clearfix">
+        <!-- footer logo -->
+        <div class="footer-text">
+            <h2>KMK ITS</h2>
+        </div>
+        <!-- social icon-->
+        <div class="footer-social-icon">
+            <a href="#" style="font-size: 0.5rem;"><i class="fab fa-line"></i></a>
+            <a href="#" style="font-size: 0.5rem;"><i class="fab fa-instagram"></i></a>
+            <a href="#" style="font-size: 0.5rem;"><i class="fab fa-facebook-square"></i></a>
+        </div>
+        <!-- Foooter Text-->
+        <div class="copyright-text">
+            <p>Copyright ©2018 KMK ITS. Designed by <a href="https://colorlib.com" target="_blank">Cete</a></p>
+        </div>
+    </footer>
+    <!-- ***** Footer Area Start ***** -->
+
+    <!-- Jquery-2.2.4 JS -->
+    <script src="js/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="js/popper.min.js"></script>
+    <!-- Bootstrap-4 Beta JS -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- All Plugins JS -->
+    <script src="js/plugins.js"></script>
+    <!-- Slick Slider Js-->
+    <script src="js/slick.min.js"></script>
+    <!-- Footer Reveal JS -->
+    <script src="js/footer-reveal.min.js"></script>
+    <!-- Active JS -->
+    <script src="js/active.js"></script>
+</body>
+
 </html>
